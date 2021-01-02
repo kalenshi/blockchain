11047778af99f973f93110eb8bd4d1820e403c4d
Kalenshi Katebe <kalenshi@gmail.com>
2021-01-02 07:12:23 -0400

    Changes to be committed: 	modified:   dev/networkNode.js 	modified:   package-lock.json 	modified:   package.json

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const rp = require('request-promise');
 const Blockchain = require('./blockchain');
-const PORT = process.argv[2]; //env.PORT || 3000;
+const PORT = process.argv[2]; //the command that we run to start the server
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -13,19 +14,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.get('/blockchain',(req, res)=>{
+app.get('/blockchain', (req, res) => {
     //send back the entire block chain
     res.send(bitcoin);
 });
 
-app.post('/transaction',(req, res)=>{
-    //the endpoint to cerate a new transaction on our block chain
+app.post('/transaction', (req, res) => {
+    //the endpoint to create a new transaction on our block chain
     const {amount, sender, recipient} = req.body;
     const blockIndex = bitcoin.createNewTransaction(amount, sender, recipient);
-    res.json({message:`The transaction will be added in block : ${blockIndex}`});
+    res.json({message: `The transaction will be added in block : ${blockIndex}`});
 });
 
-app.get('/mine',(req, res)=>{
+app.get('/mine', (req, res) => {
     //mine or create a new block for us
     const previousBlockHash = bitcoin.getLastBlock().hash;
     const currentBlockData = {
@@ -41,22 +42,45 @@ app.get('/mine',(req, res)=>{
         message: "New block mined successfully",
         newBlock: minedBlock
     });
- 
+
 });
 
-app.post('/register-and-braodcast-node', (req, res)=>{
+/**
+ *
+ */
+app.post('/register-and-braodcast-node', (req, res) => {
     //register a node and broadcast that node to the entire network
+    const regNodePromises = [];
     const {newNodeUrl} = req.body;
+    if (bitcoin.networkNodes.indexOf(newNodeUrl) === -1) {
+        bitcoin.networkNodes.push(newNodeUrl);
+    }
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: `${networkNodeUrl}/register-node`,
+            method: "POST",
+            body: {newNodeUrl},
+            json: true
+        };
+        regNodePromises.push(rp(requestOptions));
+    });
+    Promise.all(regNodePromises)
+        .then(data => {
+
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
-app.post('/register-node', (req, res)=>{
+app.post('/register-node', (req, res) => {
 //register a node with the network
 });
 
-app.post('/register-nodes-bulk', (req, res)=>{
+app.post('/register-nodes-bulk', (req, res) => {
     //register multiple nodes at once
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Now listening on Port: ${PORT}...`);
 });
